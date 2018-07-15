265ce4eaee5d0a34580ffd7f173b37839567f58e
okieselbach <git@outlook.com>
2018-07-15 21:55:08 +0200

    Initial commit AutoPilot Script

---
+++ Get-WindowsAutoPilotInfo.ps1
@@ -0,0 +1,190 @@
+<#PSScriptInfo
+
+.VERSION 1.3
+
+.GUID ebf446a3-3362-4774-83c0-b7299410b63f
+
+.AUTHOR Michael Niehaus
+
+.COMPANYNAME Microsoft
+
+.COPYRIGHT 
+
+.TAGS Windows AutoPilot
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+Version 1.0:  Original published version.
+Version 1.1:  Added -Append switch.
+Version 1.2:  Added -Credential switch.
+Version 1.3:  Added -Partner switch.
+
+#>
+
+<#
+.SYNOPSIS
+Retrieves the Windows AutoPilot deployment details from one or more computers
+.DESCRIPTION
+This script uses WMI to retrieve properties needed by the Microsoft Store for Business to support Windows AutoPilot deployment.
+.PARAMETER Name
+The names of the computers.  These can be provided via the pipeline (property name Name or one of the available aliases, DNSHostName, ComputerName, and Computer).
+.PARAMETER OutputFile
+The name of the CSV file to be created with the details for the computers.  If not specified, the details will be returned to the PowerShell
+pipeline.
+.PARAMETER Append
+Switch to specify that new computer details should be appended to the specified output file, instead of overwriting the existing file.
+.PARAMETER Credential
+Credentials that should be used when connecting to a remote computer (not supported when gathering details from the local computer).
+.PARAMETER Partner
+Switch to specify that the created CSV file should use the schema for Partner Center (using serial number, make, and model).
+.EXAMPLE
+.\Get-WindowsAutoPilotInfo.ps1 -ComputerName MYCOMPUTER -OutputFile .\MyComputer.csv
+.EXAMPLE
+.\Get-WindowsAutoPilotInfo.ps1 -ComputerName MYCOMPUTER -OutputFile .\MyComputer.csv -Append
+.EXAMPLE
+.\Get-WindowsAutoPilotInfo.ps1 -ComputerName MYCOMPUTER1,MYCOMPUTER2 -OutputFile .\MyComputers.csv
+.EXAMPLE
+Get-ADComputer -Filter * | .\GetWindowsAutoPilotInfo.ps1 -OutputFile .\MyComputers.csv
+.EXAMPLE
+Get-CMCollectionMember -CollectionName "All Systems" | .\GetWindowsAutoPilotInfo.ps1 -OutputFile .\MyComputers.csv
+.EXAMPLE
+.\Get-WindowsAutoPilotInfo.ps1 -ComputerName MYCOMPUTER1,MYCOMPUTER2 -OutputFile .\MyComputers.csv -Partner
+
+#>
+
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory=$False,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0)][alias("DNSHostName","ComputerName","Computer")] [String[]] $Name = @($env:ComputerName),
+	[Parameter(Mandatory=$False)] [String] $OutputFile = "", 
+	[Parameter(Mandatory=$False)] [Switch] $Append = $false,
+	[Parameter(Mandatory=$False)] [System.Management.Automation.PSCredential] $Credential = $null,
+	[Parameter(Mandatory=$False)] [Switch] $Partner = $false,
+	[Parameter(Mandatory=$False)] [Switch] $Force = $false
+)
+
+Begin
+{
+	# Initialize empty list
+	$computers = @()
+}
+
+Process
+{
+	foreach ($comp in $Name)
+	{
+		$bad = $false
+
+		# Get the common properties.
+		Write-Verbose "Checking $comp"
+		$serial = (Get-WmiObject -ComputerName $comp -Credential $Credential -Class Win32_BIOS).SerialNumber
+
+		# Get the hash (if available)
+		$devDetail = (Get-WMIObject -ComputerName $comp -Credential $Credential -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
+		if ($devDetail -and (-not $Force))
+		{
+			$hash = $devDetail.DeviceHardwareData
+		}
+		else
+		{
+			$bad = $true
+			$hash = ""
+		}
+
+		# If the hash isn't available, get the make and model
+		if ($bad -or $Force)
+		{
+			$cs = Get-WmiObject -ComputerName $comp -Credential $Credential -Class Win32_ComputerSystem
+			$make = $cs.Manufacturer.Trim()
+			$model = $cs.Model.Trim()
+			if ($Partner)
+			{
+				$bad = $false
+			}
+		}
+		else
+		{
+			$make = ""
+			$model = ""
+		}
+
+		# Getting the PKID is generally problematic for anyone other than OEMs, so let's skip it here
+		$product = ""
+
+		# Depending on the format requested, create the necessary object
+		if ($Partner)
+		{
+			# Create a pipeline object
+			$c = New-Object psobject -Property @{
+				"Device Serial Number" = $serial
+				"Windows Product ID" = $product
+				"Hardware Hash" = $hash
+				"Manufacturer name" = $make
+				"Device model" = $model
+			}
+			# From spec:
+			#	"Manufacturer Name" = $make
+			#	"Device Name" = $model
+
+		}
+		else
+		{
+			# Create a pipeline object
+			$c = New-Object psobject -Property @{
+				"Device Serial Number" = $serial
+				"Windows Product ID" = $product
+				"Hardware Hash" = $hash
+			}
+		}
+
+		# Write the object to the pipeline or array
+		if ($bad)
+		{
+			# Report an error when the hash isn't available
+			Write-Error -Message "Unable to retrieve device hardware data (hash) from computer $comp" -Category DeviceError
+		}
+		elseif ($OutputFile -eq "")
+		{
+			$c
+		}
+		else
+		{
+			$computers += $c
+		}
+
+	}
+}
+
+End
+{
+	if ($OutputFile -ne "")
+	{
+		if ($Append)
+		{
+			if (Test-Path $OutputFile)
+			{
+				$computers += Import-CSV -Path $OutputFile
+			}
+		}
+		if ($Partner)
+		{
+			$computers | Select "Device Serial Number", "Windows Product ID", "Hardware Hash", "Manufacturer name", "Device model" | ConvertTo-CSV -NoTypeInformation | % {$_ -replace '"',''} | Out-File $OutputFile
+			# From spec:
+			# $computers | Select "Device Serial Number", "Windows Product ID", "Hardware Hash", "Manufacturer Name", "Device Name" | ConvertTo-CSV -NoTypeInformation | % {$_ -replace '"',''} | Out-File $OutputFile
+		}
+		else
+		{
+			$computers | Select "Device Serial Number", "Windows Product ID", "Hardware Hash" | ConvertTo-CSV -NoTypeInformation | % {$_ -replace '"',''} | Out-File $OutputFile
+		}
+	}
+}
---
+++ Get-WindowsAutoPilotInfoAndUpload.ps1
@@ -0,0 +1,109 @@
+# Supporting archive files needs to be in Blob Storage
+#
+# Get-WindowsAutoPilotInfo.ps1
+# AzCopy.zip
+
+Function Start-Command {
+    Param([Parameter (Mandatory=$true)]
+          [string]$Command, 
+          [Parameter (Mandatory=$true)]
+          [string]$Arguments)
+
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $Command
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.CreateNoWindow = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $Arguments
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+    [pscustomobject]@{
+        stdout = $p.StandardOutput.ReadToEnd()
+        stderr = $p.StandardError.ReadToEnd()
+        ExitCode = $p.ExitCode  
+    }
+}
+
+# base parameters
+$containerUrl = "https://ZZZZ.blob.core.windows.net"
+$blobStorageResources = "resources"
+$blobStorageHashes = "hashes"
+$sasToken = "XXXX"
+
+$scriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
+$fileName = "$env:computername.csv"
+$outputPath = Join-Path $env:windir "temp\AutoPilotScript"
+$outputFile = Join-Path $outputPath $fileName
+
+if (-not (Test-Path $outputPath)) {
+    New-Item -Path $outputPath -ItemType Directory | Out-Null
+}
+
+# define to search in current script directory for needed files
+$autoPilotScript = Join-Path $scriptPath "Get-WindowsAutoPilotInfo.ps1"
+$azCopyExe = Join-Path $scriptPath "AzCopy.exe"
+
+if (-not (Test-Path $autoPilotScript)) {
+    # download Get-WindowsAutoPilotInfo.ps1 from BlobStorage
+    Start-BitsTransfer -Source "$containerUrl/$blobStorageResources/Get-WindowsAutoPilotInfo.ps1" -Destination $outputPath
+
+    # re-define variable to output path as we downloaded the files just in time
+    $autoPilotScript = Join-Path $outputPath "Get-WindowsAutoPilotInfo.ps1"
+}
+
+# Gather the AutoPilot Hash information
+Start-Command -Command "$psHome\powershell.exe" -Arguments "-ex bypass -file `"$autoPilotScript`" -ComputerName $env:computername -OutputFile `"$outputFile`"" | Out-Null
+
+if (-not (Test-Path $azCopyExe)) {
+    # download AzCopy from BlobStorage
+    Start-BitsTransfer -Source "$containerUrl/$blobStorageResources/AzCopy.zip" -Destination $outputPath
+    
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::ExtractToDirectory($(Join-Path $outputPath "AzCopy.zip"), $(Join-Path $outputPath "AzCopy"))
+
+    # re-define variable to output path as we downloaded the files just in time
+    $azCopyExe = Join-Path $outputPath "AzCopy\AzCopy.exe"
+}
+
+# Copy the hash information to the Blob Storage
+$url = "$containerUrl/$blobStorageHashes"
+$result = Start-Command -Command "`"$azCopyExe`"" -Arguments "/Source:`"$outputPath`" /Dest:$url /Pattern:$fileName /Y /Z:`"$(Join-Path $outputPath "AzCopy")`" /DestSAS:`"$sasToken`""
+
+
+# We try to copy the hash information to the scriptpath as the device might be installed from MDT offline media
+# ScriptPath would be pointing to the removable media for the offline install. In failure case we could use the gathered
+# information from the offline media to try the upload again.
+# this can easily be disabled this by defining $offlineMediaCopy = $false
+$offlineMediaCopy = $true
+if ($offlineMediaCopy) {
+    try {
+        if ($result.stdout.Contains("Transfer successfully:   1")) {
+            $path = $(Join-Path $scriptPath "autopilot-script-success")
+            if (-not (Test-Path $path)) {
+                New-Item -Path $path -ItemType Directory | Out-Null
+            }
+            Copy-Item -Path $outputFile -Destination $path -Force -ErrorAction SilentlyContinue | Out-Null
+        }
+        else {
+            $path = $(Join-Path $scriptPath "autopilot-script-failed")
+            if (-not (Test-Path $path)) {
+                New-Item -Path $path -ItemType Directory | Out-Null
+            }
+            Copy-Item -Path $outputFile -Destination $path -Force -ErrorAction SilentlyContinue | Out-Null
+        }
+    }
+    catch [system.exception]
+    {}
+}
+
+
+# Cleanup
+Remove-Item -Path $(Join-Path $outputPath "AzCopy.zip") -Force | Out-Null
+Remove-Item -Path $(Join-Path $outputPath "AzCopy") -Recurse -Force | Out-Null
+
+# we keep a copy of the hash in the filesystem (temp) just in case something went wrong, you can uncomment and delete the hash information also
+#Remove-Item -Path $(Join-Path $scriptPath "Get-WindowsAutoPilotInfo.ps1") -Force | Out-Null
+#Remove-Item -Path $outputFile -Force | Out-Null
