6190f9dfac198bad6ab6a1475f5eac6c0c0f36a2
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2020-03-03 17:02:18 +0100

    Initial commit
     DisablePromptOnSecureDesktop.ps1 added to the repo

---
+++ DisablePromptOnSecureDesktop.ps1
@@ -0,0 +1,35 @@
+<#
+Version: 1.0
+Author: Oliver Kieselbach
+Script: DisablePromptOnSecureDesktop.ps1
+
+Description:
+The script disables the UAC prompt on the secure desktop. 
+The deactivation enables tools like Quick Assist to work with elevation (UAC) prompts.
+
+Release notes:
+Version 1.0: Original published version. 
+
+The script is provided "AS IS" with no warranties.
+#>
+
+$PromptOnSecureDesktop = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
+
+if ($PromptOnSecureDesktop -ne 0) {
+    $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\"
+    $registryKey = "PromptOnSecureDesktop"
+    $registryValue = 0
+    
+    try
+    {
+        Set-ItemProperty -Path $registryPath -Name $registryKey -Value $registryValue -ErrorAction Stop
+        $exitCode = 0
+    }
+    catch
+    {   
+        Write-Error -Message "Could not write regsitry value" -Category OperationStopped
+        $exitCode = -1
+    }
+}
+
+exit $exitCode
