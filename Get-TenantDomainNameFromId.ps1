597f29e84322a691c42cf4bd0994a07678856573
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2021-06-01 12:00:11 +0200

    added Get-TenantDomainName

---
+++ Get-TenantDomainNameFromId.ps1
@@ -0,0 +1,11 @@
+# Get tenant domain name for tenant id
+
+# based on work from @jseerden
+# from this Twitter thread
+# https://twitter.com/joslieben/status/1394587924130418692
+
+$tenantid = "type-your-tenant-id-here"
+$authInfo = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantid/oauth2/authorize?client_id=c44b4083-3bb0-49c1-b47d-974e53cbdf3c" -Method GET
+([regex]::match($authInfo, '"UserIdLabel":"(\D+)",').Groups[1].Value).Split("@")[1]
+
+# result should include the domain name :-)
