4838dcea9c18e55d973c6a8271589d5420fe6fc6
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2021-06-18 12:00:06 +0200

    added MDM sync script

---
+++ Start-MdmSync.ps1
@@ -0,0 +1,6 @@
+﻿# Author: Oliver Kieselbach (oliverkieselbach.com)
+# The script is provided "AS IS" with no warranties.
+
+[Windows.Management.MdmSessionManager,Windows.Management,ContentType=WindowsRuntime]
+$session = [Windows.Management.MdmSessionManager]::TryCreateSession()
+$session.StartAsync()
