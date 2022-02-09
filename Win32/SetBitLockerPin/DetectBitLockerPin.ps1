35f7cc1ecb66742f46ee24a681530375c7cd5b75
Daniel Vazome <46573198+vazome@users.noreply.github.com>
2022-02-10 02:43:32 +0300

    Update DetectBitLockerPin.ps1

--- Win32/SetBitLockerPin/DetectBitLockerPin.ps1
+++ Win32/SetBitLockerPin/DetectBitLockerPin.ps1
@@ -1,3 +1,4 @@
+#You probably should specify error code 2 as fail in "Specify return codes to indicate post-installation behavior"
 $exitCode = 0
 $DetectBitlockerPin = Write-Output $(Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector | Where { $_.KeyProtectorType -eq 'TpmPin' }
 if ($DetectBitlockerPin){
